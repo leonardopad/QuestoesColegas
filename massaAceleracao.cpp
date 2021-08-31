aedba4ed7c9b691df0c2788cce6040453b266514
leonardopad <77343517+leonardopad@users.noreply.github.com>
2021-08-31 01:25:50 -0300

    Add files via upload

---
+++ massaAceleracao.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+	
+	int massa = 20;
+	int forcaResultante = 220;
+	float aceleracao = 0;
+	float fr = 0;
+	aceleracao = forcaResultante / massa;
+	fr = massa * aceleracao;
+	std:: cout << "A aceleracao adquirida e equivalente a " << aceleracao;
+	
+	
+	
+	
+}

41af9a8d8f38ee0f3aa64e724175de71deb01e3c
leonardopad <77343517+leonardopad@users.noreply.github.com>
2021-09-01 19:49:01 -0300

    Add files via upload

---
+++ DivisaoNumeros.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main (){
+	
+	int numero = 0;
+	int divisor1 = 0;
+	int divisor2 = 0;
+	
+	std:: cout << "Digite um numero: \n";
+	std:: cin >> numero;
+	std:: cout << "Digite um numero que divida o antes escolhido: ";
+	std:: cin >> divisor1;
+	std:: cout << "Digite outro numero que divida o antes escolhido: ";
+	std:: cin >> divisor2;
+	
+	if (numero % divisor1 == 0 && numero % divisor2 == 0){
+			std:: cout << "Os divisores " << divisor1 << " e " << divisor2 << " dividem " << numero;
+	}else if (numero % divisor1 != 0 && numero % divisor2 == 0){
+		std::  cout << "Que pena " << divisor1 << " nao divide " << numero << "\nmas " << divisor2 << " divide " << numero;
+	}else if (numero % divisor1 == 0 && numero % divisor2 != 0){
+		std:: cout<< "Que pena " << divisor1 << "  divide " << numero << "\nmas " << divisor2 << " nao divide " << numero;
+	}else {
+		std:: cout << "Que pena " << divisor1 << " e " <<  divisor2 << " nao dividem " << numero;
+	}
+	
+	
+	return 0;
+}
